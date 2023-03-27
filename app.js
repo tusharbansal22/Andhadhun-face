dfeacca656b86bc0a02ebd059e94e0abb59c928f
tusharb <tusharbansal@iitbhilai.ac.in>
2023-03-27 10:21:22 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const { PythonShell } = require('python-shell');
 const NodeWebcam = require("node-webcam");
 const { spawn } = require('child_process');
+const { exec } = require('child_process');
 
 const Webcam = NodeWebcam.create({});
 
@@ -17,14 +18,25 @@ app.listen(3000,()=>{
     } else {
       console.log(`Image captured and saved to ${filePath}`);
 
-      const args = ['Tushar.jpg', 'my-image.jpg'];
-      const pythonProcess = spawn('python3', ['my_script.py', ...args]);
-      pythonProcess.stdout.on('data', (data) => {
-        console.log(`Python function output: ${data}`);
-      });
-      pythonProcess.on('close', (code) => {
-        console.log(`Python process exited with code ${code}`);
-      });
+      // const args = ['Tushar.jpg', 'my-image.jpg'];
+      // const pythonProcess = spawn('python3', ['my_script.py', ...args]);
+      // pythonProcess.stdout.on('data', (data) => {
+      //   console.log(`Python function output: ${data}`);
+      // });
+      // pythonProcess.on('close', (code) => {
+      //   console.log(`Python process exited with code ${code}`);
+      // });
+
+      const cmd = `python3 script.py Tushar.jpg my-image.jpg`;
+
+exec(cmd, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  // console.error(`stderr: ${stderr}`);
+});
     }
   });
 });
--- script.py
+++ script.py
@@ -9,11 +9,13 @@ def image_check(file1,file2):
     known_encoding = face_recognition.face_encodings(known_image)[0]
 
     unknown_image = face_recognition.load_image_file(file2)
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    try:
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    except :
+        return True
 
     results = face_recognition.compare_faces([known_encoding], unknown_encoding)
     return results[0]
 sys.stdout.write(str(image_check(arg1,arg2)))
-# sys.stdout.write('Hello, world')
 # sys.stdout.flush()
-print(image_check(arg1,arg2))
+# print(image_check(arg1,arg2))
